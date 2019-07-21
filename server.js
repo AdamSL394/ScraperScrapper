1edd74e4ca9ec23e70f15b431d20b9f3fbbe3463
Adam <lehreradam@yahoo.com>
2019-07-20 17:43:37 -0700

    linked express routes to server

--- server.js
+++ server.js
@@ -25,6 +25,9 @@ var exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
+var routes = require("./controllers/scraper.js")
+
+app.use(routes);
 
 
 
