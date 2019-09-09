9188dee6e43e2f724dcdaa3b8f2801cc0ca1e7aa
Adam <lehreradam@yahoo.com>
2019-09-09 11:15:47 -0700

    linked external stylesheet

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// app.use(express.static("view"));
+app.use(express.static("public"));
 
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
